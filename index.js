340ce6cc21b02f485a272aeef4295254b47c968b
Komla Adzam <adzamkomla.dev@gmail.com>
2021-10-31 15:43:59 +0000

    Add course-info route

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => res.send('Server is okay!'));
+app.get("/", (req, res) => res.send("Server is okay!"));
 
-app.listen(3000, () => console.log('Now listening on port 3000'));
+app.post("/course-info", (req, res) => {
+  const { course } = req.body;
+
+  if (!course) {
+    return res.status(400).send({ message: "'course' is required" });
+  }
+
+  let coursePattern =
+    /^(?<department>[A-Z]+)[-|\s|:]?(?<course_number>[\d]+)\s(?<semester>Fall|F|Winter|W|Spring|S|Summer|Su)\s*(?<year>\d{2}|\d{4})$/;
+  let match = course.match(coursePattern, "i");
+
+  if (!match) {
+    coursePattern =
+      /^(?<department>[A-Z]+)[-|\s|:]?(?<course_number>[\d]+)\s(?<year>\d{2}|\d{4})\s*(?<semester>Fall|F|Winter|W|Spring|S|Summer|Su)$/;
+    match = course.match(coursePattern, "i");
+
+    if (!match) {
+      return res.status(400).json({ message: "Course is invalid" });
+    }
+  }
+  
+  return res.json(match.groups);
+});
+
+app.listen(3000, () => console.log("Now listening on port 3000"));
